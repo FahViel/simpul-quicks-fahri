c31645de38305b366a05a547348a2327a50fddb1
FahViel <fahrimaulana007@gmail.com>
2023-02-03 21:22:27 +0700

    Testing Work space

---
+++ quicks-app/src/ChatBox.js
@@ -0,0 +1,18 @@
+import React from "react";
+import '.././App.css'
+
+
+function Test() {
+    return ( 
+        <div>
+            <div className="test">
+                <h1>Hello World</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Test;
+
+
+
